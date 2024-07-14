db2ba640f3936e742002aee5424e577d99ada202
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-07-14 16:50:21 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const { Server } = require("socket.io");
 
 const io = new Server(8000, {
-  cors: true,
+  cors: {
+    origin: "https://drishti-rks.vercel.app/", // Allow requests from any origin, adjust as needed for security
+    methods: ["GET", "POST"],
+  },
 });
 
 const emailToSocketIdMap = new Map();
