210f6bd02806686389e9141318f91ba26ef9033f
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-07-14 16:49:47 +0530

    chages made

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://drishti-one.vercel.app/"), []);
+  const socket = useMemo(() => io("https://drishti-one.vercel.app:8000"), []);
 
   return (
     <SocketContext.Provider value={socket}>
