aaeb02510e7cfc8a1baeca62cc8d698c10fc1561
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2024-07-15 21:15:36 +0530

    Update codebase

--- server/index.js
+++ server/index.js
@@ -1,8 +1,57 @@
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
-const io = new Server(8000, {
+// const io = new Server(8000, {
+//   cors: {
+//     origin: "https://drishti-rks.vercel.app/", // Allow requests from any origin, adjust as needed for security
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// const emailToSocketIdMap = new Map();
+// const socketidToEmailMap = new Map();
+
+// io.on("connection", (socket) => {
+//   console.log(`Socket Connected`, socket.id);
+//   socket.on("room:join", (data) => {
+//     const { email, room } = data;
+//     emailToSocketIdMap.set(email, socket.id);
+//     socketidToEmailMap.set(socket.id, email);
+//     io.to(room).emit("user:joined", { email, id: socket.id });
+//     socket.join(room);
+//     io.to(socket.id).emit("room:join", data);
+//   });
+
+//   socket.on("user:call", ({ to, offer }) => {
+//     io.to(to).emit("incomming:call", { from: socket.id, offer });
+//   });
+
+//   socket.on("call:accepted", ({ to, ans }) => {
+//     io.to(to).emit("call:accepted", { from: socket.id, ans });
+//   });
+
+//   socket.on("peer:nego:needed", ({ to, offer }) => {
+//     console.log("peer:nego:needed", offer);
+//     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+//   });
+
+//   socket.on("peer:nego:done", ({ to, ans }) => {
+//     console.log("peer:nego:done", ans);
+//     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+//   });
+// });
+const http = require('http');
+const { Server } = require('socket.io');
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Socket.io Server Running\n');
+});
+
+const io = new Server(server, {
   cors: {
-    origin: "https://drishti-rks.vercel.app/", // Allow requests from any origin, adjust as needed for security
+    origin: "https://drishti-rks.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
@@ -10,32 +59,37 @@ const io = new Server(8000, {
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`Socket Connected`, socket.id);
-  socket.on("room:join", (data) => {
+  
+  socket.on('room:join', (data) => {
     const { email, room } = data;
     emailToSocketIdMap.set(email, socket.id);
     socketidToEmailMap.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
+    io.to(room).emit('user:joined', { email, id: socket.id });
     socket.join(room);
-    io.to(socket.id).emit("room:join", data);
+    io.to(socket.id).emit('room:join', data);
   });
 
-  socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  socket.on('user:call', ({ to, offer }) => {
+    io.to(to).emit('incomming:call', { from: socket.id, offer });
   });
 
-  socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  socket.on('call:accepted', ({ to, ans }) => {
+    io.to(to).emit('call:accepted', { from: socket.id, ans });
   });
 
-  socket.on("peer:nego:needed", ({ to, offer }) => {
-    console.log("peer:nego:needed", offer);
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  socket.on('peer:nego:needed', ({ to, offer }) => {
+    console.log('peer:nego:needed', offer);
+    io.to(to).emit('peer:nego:needed', { from: socket.id, offer });
   });
 
-  socket.on("peer:nego:done", ({ to, ans }) => {
-    console.log("peer:nego:done", ans);
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  socket.on('peer:nego:done', ({ to, ans }) => {
+    console.log('peer:nego:done', ans);
+    io.to(to).emit('peer:nego:final', { from: socket.id, ans });
   });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
